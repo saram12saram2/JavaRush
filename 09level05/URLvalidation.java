cd0405dbc7ca5fa3fd3db64712f8cc6c17998f6d
Your Name <you@example.com>
2024-01-11 17:10:51 +0600

    url

---
+++ 09level05/URLvalidation.java
@@ -0,0 +1,41 @@
+package en.javarush.task.pro.task09.task0912;
+
+/* 
+URL validation
+*/
+
+public class URLvalidation {
+    public static void main(String[] args) {
+        String[] urls = {"https://javarush.us", "https://google.com", "http://wikipedia.org", "facebook.com", "https://instagram", "codegym.cc"};
+        for (String url : urls) {
+            String protocol = checkProtocol(url);
+            String domainExtension = checkDomainExtension(url);
+
+            System.out.println("URL address: " + url + ", network protocol - " + protocol + ", domain extension - " + domainExtension);
+        }
+    }
+
+    public static String checkProtocol(String url) {
+        if (url.startsWith("https")) {
+            return "https";
+        } else if (url.startsWith("http")) {
+            return "http";
+        } else {
+            return "unknown";
+        }
+    }
+
+    public static String checkDomainExtension(String url) {
+        if (url.endsWith("com")) {
+            return "com";
+        } else if (url.endsWith("net")) {
+            return "net";
+        } else if (url.endsWith("org")) {
+            return "org";
+        } else if (url.endsWith("us")) {
+            return "us";
+        } else {
+            return "unknown";
+        }
+    }
+}
